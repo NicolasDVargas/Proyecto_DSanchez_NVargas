9d3c2bfbd400e3e29f3fa50390462712da92c8a6
Nicolas Daniel Vargas Ortiz <nicroleto1971@gmail.com>
2022-03-19 13:24:08 -0500

    commit on'master'

---
+++ src/app/Components/editar-dulce/editar-dulce.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Dulce } from 'src/app/models/candy.model';
+import { InventarioService } from 'src/app/servicios/inventario.service';
+
+@Component({
+  selector: 'app-editar-dulce',
+  templateUrl: './editar-dulce.component.html',
+  styleUrls: ['./editar-dulce.component.css']
+})
+export class EditarDulceComponent implements OnInit {
+
+  public dulce:Dulce;
+  constructor(public _inventService:InventarioService, public router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  Editar(){
+    this._inventService.editar(this.dulce);
+  }
+
+  cancelar(){
+    this.router.navigateByUrl('./inventario');
+    localStorage.clear();
+  }
+
+}
